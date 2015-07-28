b1bf64ee4bcecadda3e2604a04b963505cbfcfbc
Vincent Chin <vincent.ch91@gmail.com>
2015-07-28 07:31:10 +0000

    Added passing test for unique tag

--- test/models/quote_test.rb
+++ test/models/quote_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class QuoteTest < ActiveSupport::TestCase
-   test "the truth" do
-     assert true
+   test "unique_tag" do 
+   	 quote = Quote.create(:author => 'Vincent Chin', :saying => 'Woop dee doo')
+   	 expected = 'VC#' + quote.id.to_s
+   	 actual = quote.unique_tag
+   	 assert_equal expected, actual
    end
 end
